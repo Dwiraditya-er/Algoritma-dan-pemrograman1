39ff24abe6292d0ebe220cbb85d3a8c25efe8a63
Dwiraditya-er <radityaaditya626@gmail.com>
2024-10-29 08:23:08 +0800

    Add files via upload

---
+++ Pertemuan 5/While.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Include jumlah bintang untuk baris pertama: ";
+    cin >> n;
+
+    int i = n;
+    while (i >= 1) {
+        int j = 1;
+        while (j <= i) {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i--;
+    }
+    return 0;
+}
